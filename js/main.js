7a40c9d3b9bd9d8828de6246fd14f3c69958d3e4
RixTerreny <riccardotrave43@gmail.com>
2022-11-03 17:25:14 +0100

    first push , first half done

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const ammessi =["riccardotrave43@gmail.com","alessandrorossi@gmail.com","gianfelicespagnagatti@gmail.com"];
+const email = document.getElementById("email");
+const btn = document.getElementById("btn");
+let verified = document.getElementById("ver");
+let ok = false;
+
+btn.addEventListener("click", function(){
+    for (let i = 0;i<ammessi.length; i++ ){
+        if (email.value == ammessi[i]){
+            ok = true;
+            break;
+        }
+    }
+
+    if (email.value == 0){
+        verified.innerHTML="Inserisci e-mail";
+    }
+    else{
+        verified.innerHTML="Questo account non può accedere";
+    }
+
+    if (ok == true){
+        verified.innerHTML="Accesso eseguito";
+    }
+    ok=false;
+})
+
+
